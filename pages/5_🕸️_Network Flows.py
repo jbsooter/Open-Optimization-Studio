99d62d54cca934ae4fbb51d33b85f545c51a900e
jbsooter <84926188+jbsooter@users.noreply.github.com>
2023-07-23 16:21:12 -0400

    try all network type

--- pages/5_🕸️_Network Flows.py
+++ pages/5_🕸️_Network Flows.py
@@ -6,7 +6,7 @@ from ortools.graph.python import min_cost_flow
 
 def build_graph(address):
     # query osm
-    st.session_state["running_graph"], st.session_state["address_coords"] = osmnx.graph_from_address(address, dist=3000, dist_type='bbox',network_type="walk",
+    st.session_state["running_graph"], st.session_state["address_coords"] = osmnx.graph_from_address(address, dist=3000, dist_type='bbox',network_type="all",
                                                              simplify=False, retain_all=False, truncate_by_edge=False, return_coords=True,
                                                              clean_periphery=True)
     st.write(st.session_state["address_coords"])
