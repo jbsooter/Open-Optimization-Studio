a57ae02a3b67104c2a065d66d2ca3935440d868f
jbsooter <jblakesooter@gmail.com>
2023-09-04 19:59:24 -0500

    Many iteratins, cost function nonlinear

--- pages/4_🏃_Running Routes.py
+++ pages/4_🏃_Running Routes.py
@@ -61,24 +61,26 @@ def cost_function(way,start_node,end_node):
     cost = 100
 
     #if length is less than 500 metres, penalize to reduce tons of turns
-    if "length" in way:
-        if int(way["length"]) < 500:
-            cost = cost + 25
+    #if "length" in way:
+    #    if int(way["length"]) < 500:
+    #        cost = cost + 25
     #if speed limit is < 30 mph, make cheaper, if > 60 mph, make expensive
     if "maxspeed" in way:
         if int(way["maxspeed"][:2]) < 30:
-            cost = cost - 5
+            cost = cost - 10
         if int(way["maxspeed"][:2]) > 50:
-            cost = cost + 5
+            cost = cost + 10
     else: #some side roads have no value
-        cost = cost -5
+        cost = cost -10
     #avoid raods
     if "highway" in way:
-        if way["highway"] in [ "motorway","primary","service","residential","tertiary","service","primary_link","motorway"]:
+        if way["highway"] in ["primary","motorway","primary_link"]:
+            cost = cost + 25
+        if way["highway"] in ["service"]:
             cost = cost + 10
         #prefer cycleways
-        if way["highway"]  in ["cycleway"]:
-            cost = cost - 3
+        if way["highway"]  in ["cycleway","track","residential","footway","tertiary"]:
+            cost = cost - 25
     #make expensive if not designated foot
     if "foot" in way:
         if way["foot"] not in ["designated","yes"]:
@@ -89,8 +91,9 @@ def cost_function(way,start_node,end_node):
         if "elevation" in end_node:
             #st.write(np.absolute ((end_node["elevation"] - start_node["elevation"])/way["length"]))
 
-            if(np.absolute ((end_node["elevation"] - start_node["elevation"])/way["length"]) > .025):
-                cost = cost + 10
+            cost = cost - 100*np.sqrt(np.absolute((end_node["elevation"] - start_node["elevation"])/way["length"]))
+            #if(np.absolute ((end_node["elevation"] - start_node["elevation"])/way["length"]) > .025):
+            #    cost = cost + 10
     #if "lit" in start_node:
     #    if start_node["lit"] == "yes":
     #        cost = cost - 100
@@ -98,88 +101,91 @@ def cost_function(way,start_node,end_node):
     return cost*way["length"]
 
 def build_route():
-    #solve
-    run_mincostflow = min_cost_flow.SimpleMinCostFlow()
-    # Define the cost and capacity for each edge
-    #convert to zero based integer ortools
-    i = 0
-    u_i = {}
-    i_u = {}
-    for u, v, data in st.session_state["running_graph"].edges(data=True):
-        #convert ids to unique integers
-        i_iter = 0
-        j_iter = 0
-        if u in u_i:
-            i_iter = u_i.get(u)
-        else:
-            i_iter = i
-            u_i[u] = i
-            i_u[i] = u
-            i += 1
+    results = []
+    for i in range(0,300):
+        #solve
+        run_mincostflow = min_cost_flow.SimpleMinCostFlow()
+        # Define the cost and capacity for each edge
+        #convert to zero based integer ortools
+        i = 0
+        u_i = {}
+        i_u = {}
+        for u, v, data in st.session_state["running_graph"].edges(data=True):
+            #convert ids to unique integers
+            i_iter = 0
+            j_iter = 0
+            if u in u_i:
+                i_iter = u_i.get(u)
+            else:
+                i_iter = i
+                u_i[u] = i
+                i_u[i] = u
+                i += 1
 
-        if v in u_i:
-            j_iter = u_i.get(v)
-        else:
-            j_iter = i
-            u_i[v] = i
-            i_u[i] = v
-            i += 1
+            if v in u_i:
+                j_iter = u_i.get(v)
+            else:
+                j_iter = i
+                u_i[v] = i
+                i_u[i] = v
+                i += 1
 
-        #define cost
-        cost = cost_function(data,st.session_state["running_graph"].nodes(data=True)[u],st.session_state["running_graph"].nodes(data=True)[v])  # or any other cost function
-        #cost = cost_func_flat(data,st.session_state["running_graph"].nodes(data=True)[u],st.session_state["running_graph"].nodes(data=True)[v])  # or any other cost function
-        capacity = 1  # arc only used once
+            #define cost
+            cost = cost_function(data,st.session_state["running_graph"].nodes(data=True)[u],st.session_state["running_graph"].nodes(data=True)[v])  # or any other cost function
+            #cost = cost_func_flat(data,st.session_state["running_graph"].nodes(data=True)[u],st.session_state["running_graph"].nodes(data=True)[v])  # or any other cost function
+            capacity = 1  # arc only used once
 
-        #add arc info and set supply 0
-        run_mincostflow.add_arc_with_capacity_and_unit_cost(i_iter, j_iter, capacity, int(cost))
-        run_mincostflow.set_node_supply(i_iter,0)
-        run_mincostflow.set_node_supply(j_iter,0)
+            #add arc info and set supply 0
+            run_mincostflow.add_arc_with_capacity_and_unit_cost(i_iter, j_iter, capacity, int(cost))
+            run_mincostflow.set_node_supply(i_iter,0)
+            run_mincostflow.set_node_supply(j_iter,0)
 
-    # set source and sink
-    source_return = osmnx.nearest_nodes(st.session_state["running_graph"],st.session_state["address_coords"][1],st.session_state["address_coords"][0])
-    run_mincostflow.set_node_supply( u_i.get(source_return),1)
+        # set source and sink
+        source_return = osmnx.nearest_nodes(st.session_state["running_graph"],st.session_state["address_coords"][1],st.session_state["address_coords"][0])
+        run_mincostflow.set_node_supply( u_i.get(source_return),1)
 
-    #get node ids that are on the last graph 1-mi of the considered area
-    result = [i for i in st.session_state["running_graph"] if i not in st.session_state["running_boundary_graph"]]
+        #get node ids that are on the last graph 1-mi of the considered area
+        result = [i for i in st.session_state["running_graph"] if i not in st.session_state["running_boundary_graph"]]
 
-    #base sink on elevation. Find the greater absolute elevation difference nodes from nboundary region, take the top 10, and randomly select one of them for min cost flow
-    current_max_elev =  0
-    sink_return = {}
+        #base sink on elevation. Find the greater absolute elevation difference nodes from nboundary region, take the top 10, and randomly select one of them for min cost flow
+        current_max_elev =  0
+        sink_return = {}
 
-    #add values to node id keys that are the elevation delta (not grade) between source and current node
-    for x in result:
-        sink_return[x] = np.absolute(st.session_state["running_graph"].nodes()[x]["elevation"] - st.session_state["running_graph"].nodes()[source_return]["elevation"])
+        #add values to node id keys that are the elevation delta (not grade) between source and current node
+        for x in result:
+            sink_return[x] = np.absolute(st.session_state["running_graph"].nodes()[x]["elevation"] - st.session_state["running_graph"].nodes()[source_return]["elevation"])
 
-    #sort by elevation
-    sink_return = dict(sorted(sink_return.items(), key=lambda item: item[1],reverse=True))
+        #sort by elevation
+        sink_return = dict(sorted(sink_return.items(), key=lambda item: item[1],reverse=True))
 
-    #select from last 100
-    sink_selected = random.choice(list(sink_return.keys())[-int(len(sink_return.keys()) / 2):])
+        #select from last 100
+        sink_selected = random.choice(list(sink_return.keys()))
 
-    #set sink to the selected
-    run_mincostflow.set_node_supply(u_i.get(sink_selected),-1) #far away place
+        #set sink to the selected
+        run_mincostflow.set_node_supply(u_i.get(sink_selected),-1) #far away place
 
-    # Run the min cost flow algorithm
-    if run_mincostflow.solve() == run_mincostflow.OPTIMAL:
-        # Print the total cost and flow on each edge
-        total_cost = run_mincostflow.optimal_cost()
-        st.write(total_cost)
-        nodes_ij = []
-        for w in range(run_mincostflow.num_arcs()):
-            u = run_mincostflow.tail(w)
-            v = run_mincostflow.head(w)
-            flow = run_mincostflow.flow(w)
-            cost = run_mincostflow.unit_cost(w)
+        # Run the min cost flow algorithm
+        if run_mincostflow.solve() == run_mincostflow.OPTIMAL:
+            # Print the total cost and flow on each edge
+            total_cost = run_mincostflow.optimal_cost()
+            st.write(total_cost)
+            nodes_ij = []
+            for w in range(run_mincostflow.num_arcs()):
+                u = run_mincostflow.tail(w)
+                v = run_mincostflow.head(w)
+                flow = run_mincostflow.flow(w)
+                cost = run_mincostflow.unit_cost(w)
 
-            if flow > 0:
-                #st.write(f'Edge ({u}, {v}): Flow = {flow}, Cost = {cost}')
-                nodes_ij.append(i_u.get(u))
-                nodes_ij.append(i_u.get(v))
+                if flow > 0:
+                    #st.write(f'Edge ({u}, {v}): Flow = {flow}, Cost = {cost}')
+                    nodes_ij.append(i_u.get(u))
+                    nodes_ij.append(i_u.get(v))
 
-        sub = st.session_state["running_graph"].subgraph(nodes_ij)
+            sub = st.session_state["running_graph"].subgraph(nodes_ij)
+            results.append([sub,source_return,sink_selected,total_cost])
 
-
-        return sub,source_return, sink_selected
+        results.sort(key = lambda row: row[3])
+        return results[0][:3]
 def main():
     user_entry = st.container()
     #initialize locaiton in session state
