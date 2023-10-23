6a42a71805bc7f9f2441974045749fa5a9c5ade1
jbsooter <84926188+jbsooter@users.noreply.github.com>
2023-10-23 13:44:17 -0500

    Update 4_🏃_Running Routes.py

--- pages/4_🏃_Running Routes.py
+++ pages/4_🏃_Running Routes.py
@@ -254,7 +254,7 @@ def main():
     address = None
 
     if map_mode:
-        m = folium.Map(location=geocoder.ip('me').latlng,zoom_start=13)
+        m = folium.Map(location=geocoder.ip('me').latlng,zoom_start=13,tiles=config.running_routes["map_tile"], attr=config.running_routes["map_tile_attr"])
 
         folium.LatLngPopup().add_to(m)
         user_location_input = streamlit_folium.st_folium(m)
