21b9f1aeec9fcb0056b40d279392075bea1ede32
jbsooter <84926188+jbsooter@users.noreply.github.com>
2023-10-23 13:41:15 -0500

    Update config.py

--- utilities/config.py
+++ utilities/config.py
@@ -70,7 +70,7 @@ running_opts = dict(
     #http://leaflet-extras.github.io/leaflet-providers/preview/
     map_tile = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
     map_tile_attr = 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});',
+,
     gpx_file_name = "route.gpx",
     out_back_node_n = 10
 )
