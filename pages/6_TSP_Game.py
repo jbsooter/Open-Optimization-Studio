d1d9cec9cd4f7467447c5ea295d09056ba287f3d
jbsooter <jblakesooter@gmail.com>
2024-02-18 10:32:16 -0600

    Instances, Config, Heuristicq
    Add/move instances to config
    create seleciton mechanism
    add instance as a column to scoreboard code
    ortools heuristic soluiton and gap

--- pages/6_TSP_Game.py
+++ pages/6_TSP_Game.py
@@ -5,59 +5,14 @@ import gspread
 import pandas as pd
 import streamlit as st
 from folium.plugins import Draw
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
 from streamlit_folium import st_folium
 from geopy.distance import geodesic
 
+import utilities.config
+
 st.set_page_config(layout='wide')
-
-instance_b = {
-    "Little Rock, Arkansas": (34.7465, -92.2896),
-    "Fayetteville, Arkansas": (36.0626, -94.1574),
-    "Fort Smith, Arkansas": (35.3872, -94.4244),
-    "Jonesboro, Arkansas": (35.8423, -90.7043),
-    "Springfield, Missouri": (37.2083, -93.2923),
-    "Kansas City, Missouri": (39.0997, -94.5786),
-    "St. Louis, Missouri": (38.6270, -90.1994),
-    "Tulsa, Oklahoma": (36.1540, -95.9928),
-    "Oklahoma City, Oklahoma": (35.4676, -97.5164),
-    "Wichita, Kansas": (37.6872, -97.3301),
-    "Topeka, Kansas": (39.0473, -95.6752)
-}
-
-instance_a = {
-    "Little Rock": (34.7465, -92.2896),
-    "Fort Smith": (35.3859, -94.3985),
-    "Fayetteville": (36.0822, -94.1719),
-    "Springdale": (36.1867, -94.1288),
-    "Jonesboro": (35.8423, -90.7043),
-    #"North Little Rock": (34.7695, -92.2608),
-    "Conway": (35.0887, -92.4421),
-    "Rogers": (36.332, -94.1185),
-    "Bentonville": (36.3728, -94.2088),
-    "Pine Bluff": (34.224, -92.0198),
-    "Benton": (34.5645, -92.5868),
-    "Hot Springs": (34.5037, -93.0552),
-    #"Sherwood": (34.8151, -92.2243),
-    "Texarkana": (33.4251, -94.0477),
-    "Russellville": (35.2784, -93.1338),
-    #"Jacksonville": (34.8668, -92.1111),
-    "Bella Vista": (36.4814, -94.273),
-    "Paragould": (36.058, -90.5132),
-    "Cabot": (34.9745, -92.0165),
-    "West Memphis": (35.1465, -90.1848),
-    "Searcy": (35.2506, -91.7362),
-    "Van Buren": (35.4362, -94.3483),
-    "El Dorado": (33.2076, -92.6663),
-    "Bryant": (34.6105, -92.4895),
-    "Maumelle": (34.8676, -92.4003),
-    "Siloam Springs": (36.1881, -94.5405),
-    "Blytheville": (35.9273, -89.9187),
-    "Forrest City": (35.0081, -90.7898),
-    "Harrison": (36.2296, -93.1077)
-}
-
-
 
 def validate_tsp_solution(polylines, cities):
     """Validate if the given set of polylines is a solution to the TSP for the given cities."""
@@ -76,29 +31,34 @@ def validate_tsp_solution(polylines, cities):
         elif incCount == 0:
             missing.append(name)
 
-
-
     if len(path) == len(cities):
         return [True, "Valid Solution"]
     else:
         return [False, "Infeasible Solution. Must Visit All Cities Once and Only Once. Missing cities:   " + str(missing)]
+
 def main():
+
+    with st.sidebar:
+        name = st.text_input("Team Name", key="team_name_tsp")
+        st.radio(label="Instance",key ="tsp_instance", options=["Arkansas","South_Central","Airports"], on_change=read_scores)
+        st.subheader("Leaderboard")
+        st.dataframe(st.session_state["leaderboard"])
+
     if "leaderboard" not in st.session_state:
         st.session_state["leaderboard"] = None
         read_scores()
-    with st.sidebar:
-        name = st.text_input("Team Name")
 
     m = folium.Map(location=[36.082157, -94.171852], zoom_start=5) #fayetteville
-    for city, coord in instance_a.items():
+    for city, coord in utilities.config.tsp_game_opts[st.session_state["tsp_instance"]].items():
         folium.Circle(location=coord, popup=city, radius=5000).add_to(m)
+        #folium.Marker(location=coord,popup=city).add_to(m)
     Draw(export=True).add_to(m)
 
     output = st_folium(m, width=1300, height=1000)
 
+    heur_result = solve_heuristic(st.session_state["tsp_instance"])
     total_distance = 0
 
-    polylines_only = []
     if output["all_drawings"] is not None:
         if len(output["all_drawings"]) != 0:
             polylines_only = []
@@ -112,29 +72,32 @@ def main():
 
             polylines_only.append(output["all_drawings"][0]["geometry"])
 
-            st.write("Total Distance (mi):   " + str(round(total_distance,3)))
-
-            val = validate_tsp_solution(polylines_only, instance_a)
-            st.write(val[1])
-            st.button("Submit Score",on_click=write_scores, args=[name, total_distance,val])
-
-            with st.sidebar:
-                st.dataframe(st.session_state["leaderboard"])
+            col1,col2,col3,col4,col5 = st.columns([1,1,1,1,1])
+            col1.write("Total Distance (mi):   " + str(round(total_distance,3)))
+            col3.write("Gap from Computer Solution: " + str(round(100*(total_distance - heur_result[2])/heur_result[2],2)) + "%")
+            val = validate_tsp_solution(polylines_only, utilities.config.tsp_game_opts[st.session_state["tsp_instance"]])
+            col2.write(val[1])
+            col4.button("Submit Score",on_click=write_scores, args=[name, total_distance,val])
 
 
 
 def write_scores(name, total_distance, val):
-    if val[0]:
-        gc = gspread.service_account_from_dict(st.secrets["google_sheets"])
+    '''Write a valid score to Google Sheets List, otherwise display error message. Regardless, retrieve a new read of scoreboard. '''
+    st.write(st.session_state["team_name_tsp"] )
+    if ((st.session_state["team_name_tsp"] != "")):
+        if val[0]:
+            gc = gspread.service_account_from_dict(st.secrets["google_sheets"])
 
-        scoreboard = gc.open("TSP").worksheet("Scores")
+            scoreboard = gc.open("TSP").worksheet("Scores")
 
-        scoreboard.append_row([name, total_distance])
-        scoreboard.sort((2,'asc'))
+            scoreboard.append_row([name, total_distance, st.session_state["tsp_instance"]])
+            scoreboard.sort((2,'asc'))
+        else:
+            st.error(val[1])
+
     else:
-        st.write(val[1])
+        st.error("Submission Error. No Team Name")
     read_scores()
-
 
 def read_scores():
     gc = gspread.service_account_from_dict(st.secrets["google_sheets"])
@@ -142,8 +105,69 @@ def read_scores():
 
     leaders = pd.DataFrame(scoreboard.get_all_records())
     leaders = leaders.drop(leaders[(leaders == 0).any(axis=1)].index)
+    leaders = leaders[leaders["Instance"] == st.session_state["tsp_instance"]]
     st.session_state["leaderboard"]= leaders
 
+@st.cache_data
+def solve_heuristic(tsp_instance):
+    def create_data_model(locations):
+        data = {}
+        data['locations'] = locations
+        data['num_locations'] = len(locations)
+        data['num_vehicles'] = 1
+        data['depot'] = 0
+        return data
 
+    def distance_matrix_haversine(locations):
+        matrix = []
+        for from_node in locations:
+            row = []
+            for to_node in locations:
+                if from_node == to_node:
+                    row.append(0)
+                else:
+                    lat1, lon1 = locations[from_node]
+                    lat2, lon2 = locations[to_node]
+                    distance = geodesic((lat1,lon1),(lat2,lon2)).miles
+                    row.append(distance)
+            matrix.append(row)
+        return matrix
+
+    data = create_data_model(utilities.config.tsp_game_opts[st.session_state["tsp_instance"]])
+    distance_matrix = distance_matrix_haversine(data['locations'])
+
+    manager = pywrapcp.RoutingIndexManager(data['num_locations'], data['num_vehicles'], data['depot'])
+    routing = pywrapcp.RoutingModel(manager)
+
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return distance_matrix[from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.SAVINGS)
+    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.AUTOMATIC)
+
+    solution = routing.SolveWithParameters(search_parameters)
+    if solution:
+        index = routing.Start(0)
+        plan_output = []
+        total_distance = 0
+        while not routing.IsEnd(index):
+            from_node = manager.IndexToNode(index)
+            plan_output.append(list(utilities.config.tsp_game_opts[st.session_state["tsp_instance"]])[manager.IndexToNode(index)])
+
+            index = solution.Value(routing.NextVar(index))
+            to_node = manager.IndexToNode(index)
+            total_distance = total_distance +  distance_matrix[from_node][to_node]
+        to_node = manager.IndexToNode(index)
+        plan_output.append(list(utilities.config.tsp_game_opts[st.session_state["tsp_instance"]])[manager.IndexToNode(index)])
+        total_distance += distance_matrix[from_node][to_node]
+        return [routing.status(), plan_output, total_distance]
+    else:
+        return 'No solution found.'
 if __name__ == "__main__":
     main()
