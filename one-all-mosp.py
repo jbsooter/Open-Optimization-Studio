408b6106af494411fae2cfaead49b056ece58db3
Blake Sooter <jblakesooter@gmail.com>
2024-05-26 16:58:41 -0400

    improvements, standardized infinity

--- one-all-mosp.py
+++ one-all-mosp.py
@@ -4,6 +4,7 @@ import queue
 import networkx as nx
 import numpy as np
 
+infinity = np.Infinity
 
 class Label:
     def __init__(self, node, costs,predecessor):
@@ -15,13 +16,13 @@ class Label:
         c = self.costs
         cc = other.costs
         for i in range(0, len(c)):
-            print(i)
             if c[i] <= cc[i]:
                 return True
             elif c[i] == cc[i]:
                 continue
             else:
                 return False
+        return False
     def dominance_check(self, other):
         cc = self.costs
         c = other.costs
@@ -29,7 +30,6 @@ class Label:
         counter = 0
 
         for i in range(0,len(c)):
-            print(i)
             if c[i] <= cc[i]:
                 if c[i] < cc[i]:
                     counter += 1
@@ -42,26 +42,27 @@ class Label:
             return False
         #still need to check non equivalence?
     def __str__(self):
-            return str(self.node) + "pred: " + str(self.predecessor)
+            return str(self.node) + "    pred: " + str(self.predecessor) + "    costs:  " + str(self.costs)
 
 
 def nextCandidateLabel(v,lastProcessedLabel,sigma, L, G):
-    l_v = Label(v, [1000000,1000000],None)
+    l_v = Label(v, [infinity, infinity],None)
 
     for u in sigma:
-        for k in [lastProcessedLabel[u,v],L[u]]:
-            #l_u = L[u][k]
-            l_new = Label(v, np.sum([L[u][-1].costs , [G.edges[(u,v)]["elevation"], G.edges[(u,v)]["length"]]], axis=0), L[u][-1])
-            lastProcessedLabel[(u,v)] = k
-            #start back at line 9
-            if l_v.dominance_check(l_new):
-                if l_new.__lt__(l_v):
-                    l_v = l_new
-                    break
-    if l_v.costs[0] != 1000000:
+        for k in range(lastProcessedLabel[(u,v)],len(L[u])):
+                print(len(L[u]))
+                l_u = L[u][k]
+                l_new = Label(v, np.sum([l_u.costs , [G.edges[(u,v)]["elevation"], G.edges[(u,v)]["length"]]], axis=0), l_u.node)
+                lastProcessedLabel[(u,v)] = k
+                #start back at line 9
+                if l_v.dominance_check(l_new):
+                    print(l_new.__lt__(l_v))
+                    if l_new.__lt__(l_v):
+                        l_v = l_new
+                        break
+    if l_v.costs[0] != infinity:
         return None
 
-    print(l_v)
     return l_v
 
 def propogate(l_v, w, H,L, G):
@@ -72,14 +73,14 @@ def propogate(l_v, w, H,L, G):
         if l_w_local.dominance_check(l_new) is False:
             dom_check = False
 
-    if (w in H) is False:
+    if dom_check:
+        if (w in H) is False:
+                heapq.heappush(H,l_new)
+
+        elif l_new.__lt__(H[w]):
+            H = [label for label in H if label.node != l_new.node]
+            heapq.heapify(H)
             heapq.heappush(H,l_new)
-
-    elif l_new.__lt__(H[w]):
-
-        H = [label for label in H if label.node == l_new.node]
-        heapq.heapify(H)
-        heapq.heappush(H,l_new)
 
 
 def one_to_all(G,source):
@@ -89,18 +90,19 @@ def one_to_all(G,source):
     L = {}
 
     for v,data in G.nodes.data():
-        L[v] = [Label(v,[0,0],-1)]
+
+        L[v] = [Label(v,[infinity, infinity],-1)]
 
     last_processed_label = {}
     for a in G.edges:
         last_processed_label[a] = 0
 
-    heapq.heappush(H, L[source][0])
+    heapq.heappush(H, Label(source,[0,0],None))
 
     #line 7
     while len(H) > 0:
         l_v_star = heapq.heappop(H)
-        print(l_v_star.node)
+        print(l_v_star)
         L[l_v_star.node].append(l_v_star)
         #heapq.heappush(H,l_v_star)
 
@@ -116,7 +118,6 @@ def one_to_all(G,source):
             sigma_plus.append(v)
 
         for w in sigma_plus:
-            ##propogate
             propogate(l_v_star,w,H,L,G)
 
     return L
@@ -128,15 +129,18 @@ def main():
 
     G = nx.DiGraph()
     #pos = {1: (0, 0), 2: (-1, 0.3), 3: (2, 0.17), 4: (4, 0.255), 5: (5, 0.03)}
-    G.add_edge(1,2,length=1,elevation=10)
-    G.add_edge(2,3,length=1,elevation=5)
-    G.add_edge(4,3,length=1,elevation=5)
-
+    G.add_edge(1,2,length=1,elevation=10000000)
+    G.add_edge(2,1,length=1,elevation=1)
+    G.add_edge(3,2,length=1,elevation=1)
+    G.add_edge(2,3,length=1,elevation=1)
+    G.add_edge(1,3,length=1,elevation=1)
+    G.add_edge(3,1,length=1,elevation=1)
     result = one_to_all(G,2)
 
     for i in result.values():
-        for ii in i:
-            print(ii)
+       for ii in i:
+           if ii.predecessor!= -1:
+                print(ii)
 
 
 
