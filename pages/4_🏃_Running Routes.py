7880126671d6928e45a56cf5239077c9f4e4546d
Blake Sooter <jblakesooter@gmail.com>
2024-06-24 18:23:16 -0400

    fix zero div with small tolerance

--- pages/4_🏃_Running Routes.py
+++ pages/4_🏃_Running Routes.py
@@ -188,10 +188,8 @@ def elevation_cost(node_a, node_b, way):
         return abs((st.session_state["running_graph"].nodes()[node_b]["elevation"]-st.session_state["running_graph"].nodes()[node_a]["elevation"])/way["length"])
     elif st.session_state["elevation_type"]   == "Steep":
         #use 1/grade to prevent negative
-        try:
-            return 1.0/abs((st.session_state["running_graph"].nodes()[node_b]["elevation"]-st.session_state["running_graph"].nodes()[node_a]["elevation"])/way["length"])
-        except ZeroDivisionError:
-            return 1.0
+
+        return 1.0/(abs((st.session_state["running_graph"].nodes()[node_b]["elevation"]-st.session_state["running_graph"].nodes()[node_a]["elevation"])/way["length"]) + .00001)
 def build_route_mosp(address,map_mode, mileage):
     if st.session_state["running_graph"] is None:
         build_graph(address, map_mode,mileage+.01)
